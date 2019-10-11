c54892fb1ddca459127803ec1c621c9c525124dc
zhuchentong <2037630@163.com>
2019-10-11 18:54:29 +0800

    update

--- src/app/shared/components/data-table/data-table.component.ts
+++ src/app/shared/components/data-table/data-table.component.ts
@@ -14,8 +14,12 @@ export class DataTableComponent implements OnInit {
   @Input() public model: any[] = []
   @Input() public height: number
   @Input() public page: PageService
+  @Input() public selection: boolean
+  @Input() public action: TemplateRef<void>;
   @Output('page-change') public pageChangeEvent: EventEmitter<any> = new EventEmitter()
   @ViewChild('table', { static: true }) public table: QlTable
+
+  private skipColumnWidthUpdate = false
 
   constructor() {
     // super()
@@ -23,12 +27,34 @@ export class DataTableComponent implements OnInit {
 
   public ngOnInit() {}
 
-  // private updateColumns = this.table.updateColumns
   public updateColumns(column: TableColumn) {
+    if (this.selection && column.modelKey === '__selection__') {
+      this.addSelectionColumn(column)
+      return
+    }
+
     this.table.updateColumns(column)
   }
 
+  private addSelectionColumn(column) {
+    this.skipColumnWidthUpdate = true
+    const table = this.table as any
+    // 重置数据
+    table.columnsWithLevel = []
+    table.columns.forEach(x => (x.index += 1))
+    table.columns.splice(0, 0, Object.assign(column, { index: 0 }))
+    table.columnsWidth.splice(0, 0, { auto: false, width: 60 })
+    this.table.model.forEach(x => (x.__selection__ = false))
+
+    table.transformColumnsData()
+  }
+
   public updateColumnsWidth(widthItem: WidthItem) {
+    if (this.selection && this.skipColumnWidthUpdate) {
+      this.skipColumnWidthUpdate = false
+      return
+    }
+
     this.table.updateColumnsWidth(widthItem)
   }
 
