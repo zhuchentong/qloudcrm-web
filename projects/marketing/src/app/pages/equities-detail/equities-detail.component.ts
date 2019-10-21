e3e740b32a3a7b15314f25f22a3587da658162dd
chenxiaoji <15929365823@139.com>
2019-10-21 14:40:45 +0800

    modify equities

--- projects/marketing/src/app/pages/equities-detail/equities-detail.component.ts
+++ projects/marketing/src/app/pages/equities-detail/equities-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service'
 import { QlMessageService } from 'qloud-angular/package/message/message.service'
-import { Location } from '@angular/common'
+import { DatePipe, Location } from '@angular/common'
 import { FormBuilder } from '@angular/forms'
 import { ModalService } from '@shared/utils'
 import { PageService } from '@core/http'
@@ -11,13 +11,13 @@ import { Router } from '@angular/router'
   selector: 'app-equities-detail',
   templateUrl: './equities-detail.component.html',
   styleUrls: ['./equities-detail.component.scss'],
-  providers: [ApiService, QlMessageService]
+  providers: [ApiService, PageService,DatePipe]
 })
 export class EquitiesDetailComponent implements OnInit {
   public back:boolean = true;
   public fileList: any[];
+  private dataPiple: DatePipe;
   public equitiesTypes: any[] =[
-    {key:'请选择',value:null},
     {key:'现金抵用券',value:1},
     {key: '现金红包券',value:2},
     {key: '流量充值包',value:3},
@@ -27,33 +27,32 @@ export class EquitiesDetailComponent implements OnInit {
     {key: '会员码',value:7}];
 
   public chargeOffType: any[] =[
-    {key:'请选择',value:null},
     {key:'支付后核销',value:1},
     {key:'核销后支付',value:2},
     {key:'赠予后核销',value:3},
   ]
 
   public provideCompany: any[] =[
-    {key:'请选择',value:null},
     {key:'总行',value:1},
     {key: '分行',value:2},
     {key: '支行网点',value:3},
     {key: '其他',value:4}];
   public pageData={
-    type:"",
-    name:"",
-    chargeOffType:"",
-    company:"",
-    upTime:"",
+    type:1,
+    name:"红包雨权益",
+    chargeOffType:"2",
+    company:1,
+    upTime:"2019-10-18",
     count:"100",
     lastCount:"10",
     expireTime:"2050-12-31",
     edesc:"支持用户购买理财产品权益",
     escope:"1",
-    createTime:"2019-10-18",
+    createTime: "2019-10-18",
     createDep:"支行",
     updateTime:"2019-10-18"
   }
+
   constructor(public location: Location,
               private apiService: ApiService,
               private fb: FormBuilder,
