73b2cc7d7fbbcbe773ea67b38e175facede2478c
zhuchentong <2037630@163.com>
2019-10-21 16:06:10 +0800

    update

--- projects/product/src/app/pages/product-list/product-list.component.ts
+++ projects/product/src/app/pages/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { ApiService } from '../../services/api.service'
 
 @Component({
   selector: 'app-product-list',
@@ -9,9 +10,59 @@ export class ProductListComponent implements OnInit {
   public catalogList: any[] = []
   public productList: any[] = []
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getProductCatalogList()
+  }
 
-  public onCatalogChange(id) {}
+  /**
+   * 选择标签类型改变
+   * @param param0
+   */
+  public onCatalogChange({ action, treeNodeID }) {
+    if (action === 'click') {
+      this.getProductList(treeNodeID)
+    }
+  }
+
+  /**
+   * 获取标签列表
+   * @param parent
+   */
+  private getProductList(parent) {
+    this.apiService.getProductList('product', parent).subscribe(list => {
+      // 生成树形结构
+      this.productList = list
+    })
+  }
+
+  private getProductCatalogList() {
+    this.apiService.getProductList('catalog').subscribe(list => {
+      // 添加label字段
+      list.forEach((x: any) => {
+        x.label = x.name
+      })
+      // 生成树形结构
+      this.catalogList = this.generateCatalogTree(list)
+    })
+  }
+
+  private generateCatalogTree(list) {
+    const root = list.filter(x => !x.parent)
+
+    const depth = node => {
+      const children = list.filter(x => x.parent === node.id)
+
+      if (children && children.length) {
+        node.children = children
+        node.children.forEach(depth)
+      } else {
+        node.leaf = true
+      }
+      return node
+    }
+
+    return root.map(depth)
+  }
 }
--- projects/product/src/app/services/api.service.ts
+++ projects/product/src/app/services/api.service.ts
@@ -2,7 +2,26 @@ import { NetService } from '@core/http'
 import { Injectable } from '@angular/core'
 import { of } from 'rxjs'
 
+import productList from '../../assets/json/product-list.json'
 @Injectable()
 export class ApiService {
   constructor(private net: NetService) {}
+
+  getProductCatalog() {}
+
+  public getProductList(type?, parent?) {
+    return of(
+      productList
+        .filter(x => type === undefined || x.type === type)
+        .filter(x => parent === undefined || x.parent === parent)
+        .map((x: any) => {
+          if (type === 'product') {
+            const target = productList.find(catalog => catalog.id === parent)
+            x.productType = target.name
+          }
+
+          return x
+        })
+    )
+  }
 }
