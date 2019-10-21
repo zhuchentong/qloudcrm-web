1509bb2e8004992489be122a238b3fc2d49926ad
zhuchentong <2037630@163.com>
2019-10-21 17:49:05 +0800

    update

--- projects/marketing/src/app/pages/activity-list/activity-list.component.ts
+++ projects/marketing/src/app/pages/activity-list/activity-list.component.ts
@@ -16,7 +16,6 @@ export class ActivityListComponent implements OnInit {
 
   getActivityList() {
     this.apiService.getActivityList().subscribe(data => {
-      console.log(data)
       this.activityList = data
     })
   }
