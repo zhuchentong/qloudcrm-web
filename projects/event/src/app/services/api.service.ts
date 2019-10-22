519cd8b3d87b40f9aa11017cf692325f16349769
chenxiaoji <15929365823@139.com>
2019-10-22 15:59:23 +0800

    新增时间处理

--- projects/event/src/app/services/api.service.ts
+++ projects/event/src/app/services/api.service.ts
@@ -1,8 +1,39 @@
 import { NetService } from '@core/http'
 import { Injectable } from '@angular/core'
 import { of } from 'rxjs'
+import EventListData from '../../assets/json/eventlist.json'
 
 @Injectable()
 export class ApiService {
   constructor(private net: NetService) {}
+
+  /**
+   *
+   */
+  public getEventList(){
+    return of(EventListData.dataList);
+  }
+
+  public getEventType(){
+    return of(EventListData.eventType);
+  }
+
+  public getTradeArea(){
+    return of(EventListData.tradeArea);
+  }
+
+  public getCustomerLevel(){
+    return of(EventListData.customerLevel);
+  }
+
+  public getEventChannel(){
+    return of(EventListData.channel);
+  }
+
+  public getActivation(){
+    return of(EventListData.activation);
+  }
+  public getRemindType(){
+    return of(EventListData.remindType);
+  }
 }
