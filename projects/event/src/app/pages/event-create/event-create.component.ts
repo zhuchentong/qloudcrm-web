c247a685d1a53f87274f49f155f04067aa236314
chenxiaoji <15929365823@139.com>
2019-10-22 17:32:18 +0800

    修改了 事件新增和 事件展示

--- projects/event/src/app/pages/event-create/event-create.component.ts
+++ projects/event/src/app/pages/event-create/event-create.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
+import { ApiService } from '../../services/api.service'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { ModalService } from '@shared/utils'
+import { PageService } from '@core/http'
+import { QlMessageService } from 'qloud-angular/package/message/message.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.component.html',
-  styleUrls: ['./event-create.component.css']
+  styleUrls: ['./event-create.component.css'],
+  providers: [ModalService,PageService]
 })
 export class EventCreateComponent implements OnInit {
+  public formGroup: FormGroup = this.fb.group({})
+  // public dataList: any[] ;
+  public eventType: any[];
+  public channel: any[];
+  public customerLevel: any[];
+  public tradeArea: any;
+  public triggerChannels: any[] = new Array();
+  public tagAttribute: any[] = new Array();
+  constructor(public location: Location,
+              private apiService: ApiService,
+              private fb: FormBuilder,
+              public modal: ModalService,
+              public pageService: PageService,
+              private message: QlMessageService,
+              private router: Router) {
+    this.apiService.getEventType().subscribe(data => {
+      this.eventType = data;
+    });
+    this.apiService.getEventChannel().subscribe(data => {
+      this.channel = data;
+    });
 
-  constructor() { }
+    this.apiService.getCustomerLevel().subscribe(data => {
+      this.customerLevel = data;
+    });
+    this.apiService.getTradeArea().subscribe(data =>{
+      this.tradeArea = data;
+    })
+  }
 
   ngOnInit() {
+  }
+
+  public onSubmit() {
+    this.message.success('创建成功')
+    this.router.navigate(['/event/event-list'], { replaceUrl: true })
   }
 
 }
