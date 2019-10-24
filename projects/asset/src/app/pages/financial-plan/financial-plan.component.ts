12a1b2dc0b1b64b860643c4c52ef6afa68988c0f
zhuchentong <2037630@163.com>
2019-10-24 11:14:19 +0800

    update

--- projects/asset/src/app/pages/financial-plan/financial-plan.component.ts
+++ projects/asset/src/app/pages/financial-plan/financial-plan.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { ApiService as CustomerService } from 'projects/customer/src/app/services/api.service'
 @Component({
   selector: 'app-financial-plan',
   templateUrl: './financial-plan.component.html',
   styleUrls: ['./financial-plan.component.scss']
 })
 export class FinancialPlanComponent implements OnInit {
+  public customerList: any[] = []
 
-  constructor() { }
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit() {
+    this.getCustomerList()
   }
 
+  getCustomerList() {
+    this.customerService.getCustomerList().subscribe(data => {
+      this.customerList = data
+    })
+  }
 }
