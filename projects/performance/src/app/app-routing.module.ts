12a4867437d8c1086ab7677767b7dd289dfcee07
zhuchentong <2037630@163.com>
2019-10-24 16:41:21 +0800

    update

--- projects/performance/src/app/app-routing.module.ts
+++ projects/performance/src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { environment } from '../environments/environment'
-import { PerformanceRankComponent } from './pages/performance-rank/performance-rank.component'
 import { PerformanceQuotaComponent } from './pages/performance-quota/performance-quota.component'
-import { PerformanceStatisticComponent } from './performance-statistic/performance-statistic.component'
+import { PerformanceStatisticComponent } from './pages/performance-statistic/performance-statistic.component'
 
 const routes: Routes = [
-  { path: 'performance/performance-rank', component: PerformanceRankComponent },
   { path: 'performance/performance-quota', component: PerformanceQuotaComponent },
   { path: 'performance/performance-statistic', component: PerformanceStatisticComponent }
 ]
 
 routes.push({
   path: environment.production ? '' : 'performance',
-  redirectTo: '/performance/performance-rank',
+  redirectTo: '/performance/performance-quota',
   pathMatch: 'full'
 })
 
